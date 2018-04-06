91b8c7580db9da2f9f1a09215f618757b5620a7a
Oscar Lovato <Oscar Lovato@LAPTOP-HN0CBI2R>
2018-04-06 12:44:35 -0600

    primera version

---
+++ src/Proveedor.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Marvin Ramirez
+ */
+public class Proveedor {
+    
+    private String Nombre;
+    private String tel;
+    public Proveedor(){};
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+    
+}
---
+++ src/producto.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Marvin Ramirez
+ */
+public class producto {
+    private String nombre;
+    private int cantidad;
+    private int precio;
+    private Proveedor proveedor;
+    
+    public producto(String nombre, int cantidad, int precio,Proveedor proveedor){
+        this.nombre=nombre;
+        this.cantidad=cantidad;
+        this.precio=precio;
+        this.proveedor=proveedor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public Proveedor getProveedor() {
+        return proveedor;
+    }
+
+    public void setProveedor(Proveedor proveedor) {
+        this.proveedor = proveedor;
+    }
+    
+}
+
